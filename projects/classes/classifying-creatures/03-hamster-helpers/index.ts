0c0d7f645cc5019d695882acf25c801d0eeeca96
helena <helenapaulasova@hotmail.com>
2023-12-28 14:15:33 +0100

    classes appetizer 3

--- projects/classes/classifying-creatures/03-hamster-helpers/index.ts
+++ projects/classes/classifying-creatures/03-hamster-helpers/index.ts
@@ -1,2 +1,60 @@
 // Write your type and classes here! ✨
+export type SmallPetFood =
+	| "bugs"
+	| "fruits"
+	| "insects"
+	| "plants"
+	| "seeds"
+	| "vegetables";
+
+export abstract class SmallFurryPet {
+	readonly species;
+	protected happiness = 0;
+
+	constructor(species: string) {
+		this.species = species;
+	}
+
+	abstract eats(food: SmallPetFood): boolean;
+
+	isHappy() {
+		return this.happiness > 0;
+	}
+}
+
+export class Gerbil extends SmallFurryPet {
+	constructor() {
+		super("Gerbil");
+	}
+
+	dig() {
+		this.happiness += 1;
+	}
+
+	eats(food: SmallPetFood) {
+		switch (food) {
+			case "insects":
+			case "plants":
+			case "seeds":
+			case "vegetables":
+				return true;
+			default:
+				return false;
+		}
+	}
+}
+
+export class Hamster extends SmallFurryPet {
+	constructor() {
+		super("Hamster");
+	}
+
+	run() {
+		this.happiness += 1;
+	}
+
+	eats() {
+		return true;
+	}
+}
 // You'll need to export the classes so the tests can run them.
