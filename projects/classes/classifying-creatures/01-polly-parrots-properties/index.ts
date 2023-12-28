65cf33644ad1f5f260e96934b7e6ed96f3d7c243
helena <helenapaulasova@hotmail.com>
2023-12-28 13:35:39 +0100

    classes appetizer 02

--- projects/classes/classifying-creatures/01-polly-parrots-properties/index.ts
+++ projects/classes/classifying-creatures/01-polly-parrots-properties/index.ts
@@ -1,2 +1,33 @@
 // Write your Parrot class here! ✨
+export class Parrot {
+    #name: string;
+    #phrases: string[] = [];
+
+    constructor(name: string) {
+        this.#name = name;
+        this.#phrases.push(`${name} wants a cracker!`);
+    }
+
+    get name() {
+        return this.#name;
+    }
+
+    get phrases() {
+        return this.#phrases;
+    }
+
+    announce() {
+        return `Squawk! I'm ${this.name}!`
+    }
+
+    learn(phrase: string) {
+        this.#phrases.push(phrase);
+    }
+
+    speak() {
+        const N = this.phrases.length;
+        const i = Math.floor(Math.random() * N);
+        return this.#phrases[i];
+    }
+}
 // You'll need to export it so the tests can run it.
--- projects/classes/classifying-creatures/02-dog-displays/index.ts
+++ projects/classes/classifying-creatures/02-dog-displays/index.ts
@@ -1,2 +1,22 @@
 // Write your class and interface here! ✨
+export interface PuppyInTheWindow {
+	readonly colors: string[];
+	readonly furriness: number;
+	readonly owner: string | undefined;
+}
+
+export class Puppy implements PuppyInTheWindow {
+	colors;
+	furriness;
+	owner: string | undefined;
+
+	constructor(colors: string[], furriness: number) {
+		this.colors = colors;
+		this.furriness = furriness;
+	}
+
+	adopt(owner: string) {
+		this.owner = owner;
+	}
+}
 // You'll need to export it so the tests can run it.
